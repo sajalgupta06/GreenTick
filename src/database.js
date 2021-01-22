141bacbaa6ee21120350ba4fbf7d883c3178189b
Saumya25mis <63418852+Saumya25mis@users.noreply.github.com>
2021-01-22 22:07:14 +0530

    Add files via upload

--- src/database.js
+++ src/database.js
@@ -2,37 +2,64 @@ export const ARRAY = [
     {
         id:1,
         name:"Question1", 
-        level:"Easy"
+        level:"Easy",
+        completed:false,
+        site:"Hackerrank",
     },
     {
         id:2,
         name:"Question2", 
-        level:"Easy"
+        level:"Easy",
+        completed:false,
+        site:"Hackerrank",
     },
     {
         id:3,
         name:"Question3", 
-        level:"Medium"
+        level:"Medium",
+        completed:false,
+        site:"LeetCode",
     },
     {
         id:4,
         name:"Question4", 
-        level:"Medium"
+        level:"Medium",
+        completed:false,
+        site:"Codeforces",
     },
     {
         id:5,
         name:"Question5", 
-        level:"Hard"
+        level:"Hard",
+        completed:false,
+        site:"Codechef",
     },
     {
         id:6,
         name:"Question6", 
-        level:"Hard"
+        level:"Hard",
+        completed:false,
+        site:"Codechef",
     },
+
 ]; 
 export const RESOURCES=[
     "Addressing1","Addressing2","Addressing3", "Addressing4","Addressing5","Addressing6","Addressing7","Addressing8"
 ];
 export const VIDEOS=[
-    "Video1","Video2"
+    {
+        id:1,
+        name:"Video1",
+        url:"xyz"
+    },
+    {
+        id:2,
+        name:"Video2",
+        url:"yxz"
+    },
+    {
+        id:3,
+        name:"Video3",
+        url:"xyz"
+    },
 ];
