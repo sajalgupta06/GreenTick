ff0d39204cb529ad549bcf0d5c6859cd4bc737ac
Saumya25mis <63418852+Saumya25mis@users.noreply.github.com>
2021-01-22 22:06:03 +0530

    Add files via upload

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,33 +1,78 @@
 import { render } from "@testing-library/react";
+
 import React, { Component } from "react";
 import { ARRAY } from "../database";
+import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+import { SiHackerrank,SiCodechef,SiLeetcode,SiCodeforces } from "react-icons/si";
+import {GrCopy} from "react-icons/gr";
+
+const SomeComponent = withRouter(props => <Array {...props}/>);
 
 class Array extends Component {
+  constructor(props){
+    super(props);
+    this.changeText = this.changeText.bind(this);
+
+    this.state={
+      ds:ARRAY
+    };
+  }
+
+   changeText(id){
+        const newIds = this.state.ds.slice() //copy the array
+        newIds[id-1].completed = !(newIds[id-1].completed) //execute the manipulations
+        this.setState({ds: newIds}) //set the new state
+        console.log(this.state)
+  }
   render() {
+    const {pathname} = this.props.location;
+    var set1=false;
+    var set2=false;
+    if(pathname=='/practice'){
+       set1= true;
+    }
+    else if(pathname=='/resource'){
+          set2=true;
+    }
     const table = ARRAY.map((ques) => {
       return (
-        <div className="container" style={{ background: "#fff" }}>
-          <div className="row" style={{    marginBottom: "1em",    marginLeft: "2.6em"}}>
-            <div className="col-2 ml-3">{ques.id}</div>
-            <div className="col-5" style={{ color: "#2C7EDE" }}>
+        <div  style={{ background: "#fff" }}>
+          <div className="row" style={{  background:ques.id%2==0?"#DFF0FD":"#FFFFFF",    margin: "auto"}}>
+            <div className="col-1 " style={{textAlign:"center",fontWeight:"600",paddingTop:"0.65em"}}>{ques.id}</div>
+            <div className="col-3" style={{ color: "#000000",textAlign:"center",fontWeight:"600",paddingTop:"0.65em",wordWrap:"break-word" }}>
               {ques.name}
             </div>
+            <a className="col-3" href="#" style={{fontSize:"130%",color:"#FFA800",paddingLeft:"4.5em",paddingTop:"0.3em"}}>&#9734;</a>
             {ques.level == "Easy" ? (
-              <div className="col-2" style={{ color: "#0FBB00",fontWeight:"600" }}>
+              <div className="col-1" style={{paddingTop:"0.5em",paddingBottom:"0.5em"}}>
+                  <div  style={{ background: "#0FBB00",height:"5vh",borderRadius:"2em",textAlign:"center", padding:"0.5em",color:"#FFFFFF" }}>
                 Easy
               </div>
+              </div>
             ) : ques.level == "Medium" ? (
-              <div className="col-2" style={{ color: "#FFA800",fontWeight:"600" }}>
+              <div className="col-1" style={{paddingTop:"0.5em",paddingBottom:"0.5em"}}>
+                <div  style={{ background: "#FFA800",height:"5vh",borderRadius:"2em",textAlign:"center",padding:"0.5em",color:"#FFFFFF"  }}>
                 Medium
               </div>
-            ) : (
-              <div className="col-2" style={{ color: "red" ,fontWeight:"600"}}>
-                Hard
               </div>
-            )}
-            <div className="col-2 ml-2">
-              <a href="#">solution</a>
+            ) : (
+              <div  className="col-1" style={{paddingTop:"0.5em",paddingBottom:"0.5em"}}> <div  style={{ background: "red" ,height:"5vh",borderRadius:"2em",textAlign:"center",padding:"0.5em",color:"#FFFFFF" }}>
+              Hard
             </div>
+            </div>
+            )}
+            <div  className="col-1" style={{paddingTop:"0.8em",textAlign:"center"}}> <input style={{fontSize:"200%"}}
+             onChange={()=>{this.changeText(ques.id)}} type="checkbox"></input></div>
+  
+            <div className="col-1" style={{textAlign:"center",paddingTop:"0.5em"}} ><GrCopy size={20}/></div>
+            {
+              ques.site =="Hackerrank"?(
+                <div className="col-1" style={{paddingTop:"0.5em"}}><SiHackerrank size={20}/></div>
+              ): ques.site =="Codechef"?(
+                <div className="col-1"  style={{paddingTop:"0.5em"}}><SiCodechef size={20}/></div>
+                ):ques.site=="LeetCode"?(<div className="col-1"  style={{paddingTop:"0.5em"}}><SiLeetcode size={20}/></div>):(<div className="col-1"  style={{paddingTop:"0.5em"}}><SiCodeforces size={20}/></div>)
+            }
           </div>
         </div>
       );
@@ -39,19 +84,19 @@ class Array extends Component {
           className="row mt-5 "
           style={{ justifyContent: "center", background: "#F1F1F1" }}
         >
-          <button
-            style={{ background: "#FFFFFF", width: "11%", marginRight: "2em" }}
+          <Link to='resources'
+            style={{ background: set2?"#000000":"#FFFFFF", width: "11%", marginRight: "2em",borderRadius:"0.5em" }}
             type="button"
-            className="btn btn-outline-dark mt-4 mb-4 mr-2"
-          >
-            <b style={{ color: "#000000" }}>Resources</b>
-          </button>
+            className="btn btn-outline-dark mt-4 mb-4 mr-2">
+            <b style={{ color: set2?"#FFFFFF":"#000000"  }}>Resources</b>
+          </Link>
           <button
-            style={{ background: "#FFFFFF", width: "11%", marginRight: "2em" }}
+            style={{ 
+              background: set1?"#000000":"#FFFFFF", width: "11%", marginRight: "2em",borderRadius:"0.5em" }}
             type="button"
             className="btn btn-outline-dark mt-4 mb-4 ml-2"
           >
-            <b style={{ color: "#000000" }}>Practice</b>
+            <b style={{ color: set1?"#FFFFFF":"#000000"  }}>Practice</b>
           </button>
         </div>
         <div
@@ -60,89 +105,54 @@ class Array extends Component {
         >
           <h2 style={{ display: "flex", justifyContent: "center",fontWeight: "900" }}>Arrays</h2>
         </div>
-        <div className="row mt-4 mb-2" style={{marginLeft: "3.5em"}} >
-          <div className="col-6 ml-5 ">Search for a Problem</div>
-          <div className="col-2 ml-5">
-            <div className="dropdown"    style={{    display: "inline-block",
-            left: "5.3em"}}>
-              <button   
-                className="btn dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-             
-              >
-                Difficulty
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Easy
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Medium
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Hard
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <div className="row" style={{marginBottom:"1em",marginTop:"0.5em"}}>
+          <div className="col-10" >
+          <div class="dropdown" style={{float:"right"}}>
+  <a class=" dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"  style={{background:"#F1F1F1",fontWeight:"500",color:"#000000",borderRadius:"0.4em",padding:"0.4em"}}>
+    Difficulty
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
           </div>
-          <div className="col-2 ml-2">
-            <div className="dropdown"  style={{    display: "inline-block",
-            left: "4.6em"}}>
-              <button
-                className="btn  dropdown-toggle"
-                type="button"
-                id="dropdownMenuButton"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style={{marginLeft: "0.8em"}}
-               
-              >
-                Tag
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <div className="col"><div class="dropdown">
+  <a class=" dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"  style={{background:"#F1F1F1",fontWeight:"500",color:"#000000",borderRadius:"0.4em",padding:"0.4em"}}>
+    Tag
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div></div>
         </div>
-        <div className="row" style={{    marginTop :"2em",marginLeft: "2.6em"}} >
-          <div className="col-2 ml-4 mb-2">
+  
+        <div className="row" style={{ background:"#636262",color:"#FFFFFF",paddingTop:"0.7em", verticalAlign:"middle", margin: "auto",justifyItems:"center"}} >
+          <div className="col-1 ml-4" style={{textAlign:"center"}}>
             <h2 style={{fontWeight:"600",fontSize:"1.2em",marginLeft: "0.4em"}}> #</h2>
           </div>
-          <div className="col-5 mb-2">
-            <h2 style={{fontSize: "1.2em" ,fontWeight:"600",    marginLeft: "0.4em"}}>Title</h2>
+          <div className="col-2" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em" ,fontWeight:"600",marginLeft:"1em",paddingLeft:"4em"}}>Title</h2>
           </div>
-          <div className="col-2 mb-2">
-            <h2 style={{fontSize: "1.2em",fontWeight:"600",}}>Difficulty</h2>
+          <div className="col-3" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em",fontWeight:"600",marginLeft:"5em"}}>Saved</h2>
+          </div>
+          <div className="col-1" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em",fontWeight:"600",marginLeft:"5.5em"}}>Difficulty</h2>
+          </div>
+          <div className="col-1" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em",fontWeight:"600",marginLeft:"6.5em"}}>Done</h2>
+          </div>
+          <div className="col-1" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em",fontWeight:"600",marginLeft:"5.5em"}}>Editorial</h2>
+          </div>
+          <div className="col-1" style={{textAlign:"center"}}>
+            <h2 style={{fontSize: "1.2em",fontWeight:"600",marginLeft:"5em"}}>Tag</h2>
           </div>
         </div>
         {table}
--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,68 +1,127 @@
 import React, { Component } from "react";
 import { RESOURCES, VIDEOS } from "../database";
+import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+
+const SomeComponent = withRouter(props => <Resource {...props}/>);
 
 class Resource extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      current:0
+    }
+  }
+
   render() {
+    const {pathname} = this.props.location;
+    var set1=false;
+    var set2=false;
+    var vari;
+    if(pathname=='/practice'){
+       set1= true;
+    }
+    else if(pathname=='/resources'){
+          set2=true;
+    }
     const table = RESOURCES.map((res) => {
       return (
         <b>
-          <li>{res}</li>
-        </b>
+        <li><span style={{marginRight:"1em"}}> &#9679;</span> {res}</li>
+      </b>
       );
     });
     const Videos = VIDEOS.map((vid) => {
+      if(vid.name=="Video1"){
+        vari=true;
+        }
       return (
         <div
-          class="card"
-          style={{ width: "25vw", height: "40vh", background: "#F1F1F1" }}
+         className={vari?"carousel-item active":"carousel-item"}
+          style={{ width: "25vw", height: "30vh", background: "#F1F1F1",marginLeft:"5em" }}
         >
-          <img src="..." class="card-img-top" alt={vid}></img>
+          <img src="..." class="card-img-top" alt={vid.name}></img>
         </div>
       );
     });
+    console.log(Videos);
     return (
       <div class="container">
         <div
-          class="row mt-5 "
+          className="row mt-5 "
           style={{ justifyContent: "center", background: "#F1F1F1" }}
         >
-          <button
-            style={{ background: "#FFFFFF" }}
+          <Link to='resources'
+            style={{  background: set2?"#000000":"#FFFFFF", width: "11%", marginRight: "2em",borderRadius:"0.5em" }}
             type="button"
-            class="btn btn-outline-dark mt-4 mb-4 mr-2"
-          >
-            <b style={{ color: "#000000" }}>Resources</b>
-          </button>
-          <button
-            style={{ background: "#FFFFFF" }}
+            className="btn btn-outline-dark mt-4 mb-4 mr-2">
+            <b style={{ color: set2?"#FFFFFF":"#000000" }}>Resources</b>
+          </Link>
+          <Link to='practice'
+            style={{  background: set1?"#000000":"#FFFFFF", width: "11%", marginRight: "2em",borderRadius:"0.5em" }}
             type="button"
-            class="btn btn-outline-dark mt-4 mb-4 ml-2"
+            className="btn btn-outline-dark mt-4 mb-4 ml-2"
           >
-            <b style={{ color: "#000000" }}>Practice</b>
-          </button>
-        </div>
-        <div class="row mt-5" style={{ justifyContent: "center" }}>
-          <h2>Arrays</h2>
+            <b style={{ color: set1?"#FFFFFF":"#000000" }}>Practice</b>
+        </Link>
         </div>
         <div
           style={{
             display: "grid",
-            gridTemplateColumns: "auto auto",
-            justifyItems: "center",
+            gridTemplateColumns:"1fr 1fr",
+            justifyItems:"center",
           }}
         >
-          {table}
+          <div style={{background:"#FFFFFF",padding:"2em",
+            borderRadius:"2em",width:"35vw",textAlign:"left"}}>
+              <h2 style={{fontWeight:"900",textAlign:"center",marginBottom:"1em"}}>Arrays</h2>
+              <div style={{paddingLeft:"3em"}}>{table}</div></div>
+          <div style={{background:"#FFFFFF",
+            borderRadius:"2em",width:"35vw",paddingTop:"6em",height:"35vw"}}>
+              <h2 style={{textAlign:"center",fontWeight:"700"}}>Title</h2>
+              <div>
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            {Videos}
         </div>
-        <div
-          class="mt-5"
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto auto",
-            justifyItems: "center",
-          }}
-        >
-          {Videos}
-        </div>
+      </div>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                 </div>
+              </div>
+    </div>
       </div>
     );
   }
